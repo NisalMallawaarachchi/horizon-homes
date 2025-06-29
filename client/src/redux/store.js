71daf623c9137c5ef3b566871d86e577e2212440
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-06-29 18:34:25 +0530

    refactor: re-enable PersistGate and streamline Redux store configuration

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,27 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage"; // localStorage
-// import { PERSIST, PURGE } from "redux-persist"; // Add PURGE for logout
+import {persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage
 
-// Persist config (only persist user data)
-// const userPersistConfig = {
-//   key: "user", // Unique key for user slice
-//   storage,
-//   version: 1,
-// };
+const rootReducer = combineReducers({
+  user: userReducer})
 
-// Persist only the user reducer
-// const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
+const persistConfig = {
+  key: "root", // Root key for persistence
+  storage, // Use localStorage for persistence
+  version: 1, // Versioning for persistence
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false
     }),
 });
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
