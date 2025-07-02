85c27423807f63400bec6da6c875d555ddfc27bb
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-07-02 10:51:41 +0530

    refactor: implement delete user functionality with authorization check

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -40,3 +40,17 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  // Ensure only the user can delete their own account
+  if (req.user._id !== req.params.id) {
+    return next(errorHandler("You can only delete your own account", 403));
+  }
+
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
