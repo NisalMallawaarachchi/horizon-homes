c1aa471f78cd1e2d17124fe329ee440521b24ae3
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-07-05 15:39:54 +0530

    feat: add getUserListings endpoint to fetch user's listings with access control

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,13 +1,13 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.send("Welcome to the server!");
 };
 
 export const updateUser = async (req, res, next) => {
-
   // Ensure only the user can update their own account
   if (req.user._id !== req.params.id) {
     return next(errorHandler("You can only update your own account", 403));
@@ -50,9 +50,28 @@ export const deleteUser = async (req, res, next) => {
   try {
     await User.findByIdAndDelete(req.params.id);
     res
-    .clearCookie("access_token")
-    .status(200).json({ message: "User deleted successfully" })
+      .clearCookie("access_token")
+      .status(200)
+      .json({ message: "User deleted successfully" });
   } catch (error) {
     next(error);
   }
 };
+
+export const getUserListings = async (req, res, next) => {
+  try {
+    // Allow access only if the logged-in user's ID matches the requested ID
+    if (req.user.id == req.params.id) {
+      return next(errorHandler("You can only view your own listings!", 403));
+    }
+
+    // Fetch listings belonging to the user
+    const listings = await Listing.find({ userRef: req.params.id });
+
+    // Return the listings
+    res.status(200).json(listings);
+  } catch (error) {
+    next(errorHandler("Failed to fetch listings", 500));
+  }
+};
+
