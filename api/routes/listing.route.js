0bb9ea9fe5ace97f42c41dff91c06bdeb9a79918
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-07-07 12:21:29 +0530

    feat: create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,15 +1,19 @@
 import express from "express";
-import { createListing } from "../controllers/listing.controller.js";
+import {
+  createListing,
+  deleteListing,
+  updateListing,
+  getListing,
+  getListings,
+} from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifiedUser.js";
-import { deleteListing } from "../controllers/listing.controller.js";
-import { updateListing } from "../controllers/listing.controller.js";
-import { getListing } from "../controllers/listing.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.put("/update/:id", verifyToken, updateListing);
-router.get("/get/:id", getListing)
+router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 export default router;
