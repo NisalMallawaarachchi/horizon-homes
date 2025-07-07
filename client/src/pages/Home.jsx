04fa987f0a956d35652675acbfd9c17241e4fd09
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-07-07 23:18:31 +0530

    change the hero image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -100,7 +100,7 @@ export default function Home() {
           </div>
           <div className="md:w-1/2 hidden md:block">
             <img 
-              src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1473&q=80" 
+              src="https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg" 
               alt="Beautiful home"
               className="rounded-xl shadow-2xl"
             />
